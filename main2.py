ff1d53954cec7fcf73ef358c071a04032df0dace
Anaboli <90907867+Anaboli@users.noreply.github.com>
2021-09-17 17:31:14 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import spacy
+nlp = spacy.load("C:\\Users\\Olive\\Desktop\\Untitled Folder")
+
+def tmf632recognition(text):
+    
+    ausgabedict = {}
+    
+    doc = nlp(text)
+    for ent in doc.ents:
+        ausgabedict[ent.text] = ent.label_
+    
+    return ausgabedict
+
+def test(text):
+    
+    return "Hello" + "Text"
+
+
+output_text = gr.outputs.Textbox(label = "Input mapped to TMF632 Variables")
+input_text = gr.inputs.Textbox(label = "Data for AI to detect TMF632 Variables")
+
+gr.Interface(tmf632recognition,input_text, output_text).launch()
